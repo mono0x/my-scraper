56470c394d444b9ba7cebfc33b29786025b0b7bc
mono <mono0x@users.noreply.github.com>
2015-07-17 22:19:43 +0900

    Refactor

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
   "fmt"
   "github.com/gorilla/feeds"
-  "github.com/PuerkitoBio/goquery"
   "net/http"
-  "strings"
 )
 
-func handleFeed(w http.ResponseWriter, feed *feeds.Feed) {
+func RenderFeed(w http.ResponseWriter, feed *feeds.Feed) {
   atom, err := feed.ToAtom()
   if err != nil {
     w.WriteHeader(http.StatusInternalServerError)
@@ -18,82 +16,26 @@ func handleFeed(w http.ResponseWriter, feed *feeds.Feed) {
   fmt.Fprintln(w, atom)
 }
 
-func getPurolandNews() (*feeds.Feed, error) {
-  doc, err := goquery.NewDocument("http://www.puroland.jp/")
-  if err != nil {
-    return nil, err
-  }
-
-  feed := &feeds.Feed{
-    Title: "最新情報 | サンリオピューロランド",
-    Link: &feeds.Link{Href: "http://www.puroland.jp/"},
-  }
-
-  var items []*feeds.Item
-  doc.Find("#newsArea ul li a").Each(func(_ int, s *goquery.Selection) {
-    title := strings.TrimSpace(s.Text())
-    link, ok := s.Attr("href")
-    if ok {
-      items = append(items, &feeds.Item{
-        Title: title,
-        Link: &feeds.Link{Href: link},
-        Id: link,
-      })
-    }
-  })
-  feed.Items = items
-
-  return feed, nil
-}
-
-func handlePurolandNews(w http.ResponseWriter, r *http.Request) {
-  feed, err := getPurolandNews()
+func HandlePurolandNews(w http.ResponseWriter, r *http.Request) {
+  feed, err := GetPurolandNews()
   if err != nil {
     w.WriteHeader(http.StatusServiceUnavailable)
     return
   }
-  handleFeed(w, feed)
+  RenderFeed(w, feed)
 }
 
-func getPurolandInfo() (*feeds.Feed, error) {
-  doc, err := goquery.NewDocument("http://www.puroland.jp/")
-  if err != nil {
-    return nil, err
-  }
-
-  feed := &feeds.Feed{
-    Title: "お知らせ | サンリオピューロランド",
-    Link: &feeds.Link{Href: "http://www.puroland.jp/"},
-  }
-
-  var items []*feeds.Item
-  doc.Find("#infoSectionArea ul li a").Each(func(_ int, s *goquery.Selection) {
-    title := strings.TrimSpace(s.Text())
-    link, ok := s.Attr("href")
-    if ok {
-      items = append(items, &feeds.Item{
-        Title: title,
-        Link: &feeds.Link{Href: link},
-        Id: link,
-      })
-    }
-  })
-  feed.Items = items
-
-  return feed, nil
-}
-
-func handlePurolandInfo(w http.ResponseWriter, r *http.Request) {
-  feed, err := getPurolandInfo()
+func HandlePurolandInfo(w http.ResponseWriter, r *http.Request) {
+  feed, err := GetPurolandInfo()
   if err != nil {
     w.WriteHeader(http.StatusServiceUnavailable)
     return
   }
-  handleFeed(w, feed)
+  RenderFeed(w, feed)
 }
 
 func main() {
-  http.HandleFunc("/puroland-info", handlePurolandInfo)
-  http.HandleFunc("/puroland-news", handlePurolandNews)
+  http.HandleFunc("/puroland-info", HandlePurolandInfo)
+  http.HandleFunc("/puroland-news", HandlePurolandNews)
   http.ListenAndServe(":13000", nil)
 }
---
+++ puroland.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "github.com/gorilla/feeds"
+  "github.com/PuerkitoBio/goquery"
+  "strings"
+)
+
+func GetPurolandNews() (*feeds.Feed, error) {
+  doc, err := goquery.NewDocument("http://www.puroland.jp/")
+  if err != nil {
+    return nil, err
+  }
+
+  feed := &feeds.Feed{
+    Title: "最新情報 | サンリオピューロランド",
+    Link: &feeds.Link{Href: "http://www.puroland.jp/"},
+  }
+
+  var items []*feeds.Item
+  doc.Find("#newsArea ul li a").Each(func(_ int, s *goquery.Selection) {
+    title := strings.TrimSpace(s.Text())
+    link, ok := s.Attr("href")
+    if ok {
+      items = append(items, &feeds.Item{
+        Title: title,
+        Link: &feeds.Link{Href: link},
+        Id: link,
+      })
+    }
+  })
+  feed.Items = items
+
+  return feed, nil
+}
+
+func GetPurolandInfo() (*feeds.Feed, error) {
+  doc, err := goquery.NewDocument("http://www.puroland.jp/")
+  if err != nil {
+    return nil, err
+  }
+
+  feed := &feeds.Feed{
+    Title: "お知らせ | サンリオピューロランド",
+    Link: &feeds.Link{Href: "http://www.puroland.jp/"},
+  }
+
+  var items []*feeds.Item
+  doc.Find("#infoSectionArea ul li a").Each(func(_ int, s *goquery.Selection) {
+    title := strings.TrimSpace(s.Text())
+    link, ok := s.Attr("href")
+    if ok {
+      items = append(items, &feeds.Item{
+        Title: title,
+        Link: &feeds.Link{Href: link},
+        Id: link,
+      })
+    }
+  })
+  feed.Items = items
+
+  return feed, nil
+}
