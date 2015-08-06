f4ff8fce00c1e9dc4a36a99a71154757ec3d576b
mono <mono0x@users.noreply.github.com>
2015-08-06 17:26:28 +0900

    Add dotenv support

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/feeds"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 )
 
 func RenderFeed(w http.ResponseWriter, feed *feeds.Feed) {
@@ -44,8 +46,15 @@ func HandleCharacterShow(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	_ = godotenv.Load()
+
 	http.HandleFunc("/puroland-info", HandlePurolandInfo)
 	http.HandleFunc("/puroland-news", HandlePurolandNews)
 	http.HandleFunc("/character-show", HandleCharacterShow)
-	http.ListenAndServe(":13000", nil)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "13000"
+	}
+	http.ListenAndServe(":" + port, nil)
 }
