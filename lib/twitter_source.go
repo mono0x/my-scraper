d829dfdfad67b66f72d7067e58b264def688530a
mono <mono@mono0x.net>
2016-12-05 15:29:51 +0900

    Add TwitterSource

---
+++ lib/twitter_source.go
@@ -0,0 +1,91 @@
+package scraper
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/gorilla/feeds"
+)
+
+type TwitterSource struct {
+	userId int64
+}
+
+// https://github.com/ChimeraCoder/anaconda/issues/101
+var (
+	lock       sync.RWMutex
+	twitterApi *anaconda.TwitterApi
+)
+
+func getTwitterApi() *anaconda.TwitterApi {
+	{
+		lock.RLock()
+		defer lock.RUnlock()
+		if twitterApi != nil {
+			return twitterApi
+		}
+	}
+
+	{
+		lock.Lock()
+		defer lock.Unlock()
+		if twitterApi != nil {
+			return twitterApi
+		}
+
+		anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
+		anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
+		twitterApi = anaconda.NewTwitterApi(
+			os.Getenv("TWITTER_OAUTH_TOKEN"), os.Getenv("TWITTER_OAUTH_TOKEN_SECRET"))
+		return twitterApi
+	}
+}
+
+func NewTwitterSource(userId int64) *TwitterSource {
+	return &TwitterSource{
+		userId: userId,
+	}
+}
+
+func (s *TwitterSource) Scrape() (*feeds.Feed, error) {
+	api := getTwitterApi()
+
+	values := url.Values{}
+	values.Set("user_id", strconv.FormatInt(s.userId, 10))
+	values.Set("count", "100")
+	timeline, err := api.GetUserTimeline(values)
+	if err != nil {
+		return nil, err
+	}
+	return s.Render(timeline)
+}
+
+func (s *TwitterSource) Render(timeline []anaconda.Tweet) (*feeds.Feed, error) {
+	if len(timeline) == 0 {
+		return nil, errors.New("timeline is empty")
+	}
+	user := timeline[0].User
+	userUrl := fmt.Sprintf("https://twitter.com/%s", user.ScreenName)
+	items := make([]*feeds.Item, 0, len(timeline))
+	for _, tweet := range timeline {
+		created, err := tweet.CreatedAtTime()
+		if err != nil {
+			continue
+		}
+		items = append(items, &feeds.Item{
+			Title:   tweet.Text,
+			Created: created,
+			Link:    &feeds.Link{Href: fmt.Sprintf("%s/status/%s", userUrl, tweet.IdStr)},
+		})
+	}
+	return &feeds.Feed{
+		Title: fmt.Sprintf("%s (@%s)", user.Name, user.ScreenName),
+		Link:  &feeds.Link{Href: userUrl},
+		Items: items,
+	}, nil
+}
---
+++ lib/twitter_source_test.go
@@ -0,0 +1,40 @@
+package scraper
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwitterSource(t *testing.T) {
+	jsonData, err := ioutil.ReadFile("data/api.twitter.com/1.1/statuses/user_timeline.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var timeline []anaconda.Tweet
+	if err := json.Unmarshal(jsonData, &timeline); err != nil {
+		t.Fatal(err)
+	}
+
+	source := NewTwitterSource(725638238291943424)
+	feed, err := source.Render(timeline)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	loc, err := time.LoadLocation("UTC")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "Sanrio Events (@sanrio_events)", feed.Title)
+	assert.Equal(t, 20, len(feed.Items))
+	assert.Equal(t, "12/24 KT 藤崎本館 (藤崎本館): https://t.co/Gh7HkC6yM2", feed.Items[0].Title)
+	assert.Equal(t, "https://twitter.com/sanrio_events/status/805532146110582784", feed.Items[0].Link.Href)
+	assert.WithinDuration(t, time.Date(2016, 12, 4, 22, 0, 4, 0, loc), feed.Items[0].Created, 0)
+}
